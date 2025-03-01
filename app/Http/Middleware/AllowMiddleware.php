7015ac8394ca0d0ddb6b22ba58741d7026015003
yaser samsam <yaser.alsamsam@gmail.com>
2025-03-02 02:04:17 +0400

    update header commit

--- app/Http/Middleware/AllowMiddleware.php
+++ app/Http/Middleware/AllowMiddleware.php
@@ -19,7 +19,8 @@ class AllowMiddleware
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            // ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Access-Control-Allow-Headers', '*')
             ->header('Accept', 'application/json,multipart/form-data');
     }
 }
