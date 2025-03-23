7316f8b7b8d6691bb43464cd872940a139a4ac74
yaser samsam <yaser.alsamsam@gmail.com>
2025-03-24 00:48:00 +0300

    get data from latest and get volunteer name with tree and workcommit

--- app/Http/Resources/AdvertisementsResource.php
+++ app/Http/Resources/AdvertisementsResource.php
@@ -15,7 +15,6 @@ class AdvertisementsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        ;
         return [
             'id'=>$this->id,
             'name'=>$this->name,
